feedfe249c9036e031850e92bb3b5761ee181676
15102777540 <1973271032@qq.com>
2019-01-08 11:44:35 +0800

    2019-1-8

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Tabbar from '@/components/Tabbar'
+import Logistics from '@/components/Logistics'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Logistics',
+      name: 'Logistics',
+      component: Logistics
     }
   ]
 })
